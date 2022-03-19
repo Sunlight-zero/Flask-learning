d3b866435b9cfd4980ee10d5734f4c07420ab592
Sunlight-qwq <2802655605@qq.com>
2022-03-19 23:17:03 +0800

    Add the login function.

--- app/models.py
+++ app/models.py
@@ -1,17 +1,34 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 # 用Python类储存数据库结构，这些类可以被ORM映射为数据库的对象
-class User(db.Model):
+# 用户类将对应sql的一个关系（二维表），而用户的实例则对应表格的一行
+class User(UserMixin, db.Model):
     '''
-    存储网站的用户信息
+    存储网站的用户信息，并使用flask login处理用户登录功能
     '''
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        '''
+        设置用户密码
+        '''
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,31 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Sunlight'}
-    return render_template('index.html', title='主页', user=user)
+    return render_template('index.html', title='主页')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('用户名或密码错误')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         flash('正在处理{}的登录请求'.format(form.username.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='登录', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
