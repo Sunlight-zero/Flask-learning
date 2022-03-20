c2ef448bd43ac05d8d41328540bfbb372074a8b1
Sunlight-qwq <2802655605@qq.com>
2022-03-20 18:48:53 +0800

    Add Chinese text to flask login module

---
+++ app/localization.py
@@ -0,0 +1,8 @@
+from app import login
+
+
+# flask login 消息汉化
+login.login_message = '该页面需要登录，请先登录'
+login.needs_refresh_message = '您的登录信息已过期，请重新登录'
+login.login_message_category = '消息'
+login.needs_refresh_message_category = '消息'
